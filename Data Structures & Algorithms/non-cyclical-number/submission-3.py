da99ca805f2d5dbb058f29b2d549cc27a88710fe
Asritp5 <154317067+Asritp5@users.noreply.github.com>
2026-07-31 10:18:42 +0530

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def myFun(self,n):
+        total=0
+        while n:
+            digit=n%10
+            total+=digit**2
+            n//=10
+        
+        return total    
+               
+    def isHappy(self, n: int) -> bool:
+        slow,fast=n,self.myFun(n)
+        power=dist=1
+
+        while slow!=fast:
+            if power==dist:
+                slow=fast
+                dist=0
+                power*=2
+
+            fast=self.myFun(fast)
+            dist+=1
+
+        return fast==1    
+            
